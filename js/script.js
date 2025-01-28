5b3abe03838b6c8966757ae11ced0cb243bd92a9
Hodrigo Dionathan <hodrigodionatha@gmail.com>
2025-01-28 15:58:14 -0300

    atee

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function comprarEbook() {
+    alert("Obrigado pelo interesse! Em breve você será redirecionado para a página de compra.");
+    // Aqui você pode adicionar o redirecionamento para a página de compra do ebook
+}
+
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    // Aqui você pode adicionar o código para enviar o formulário
+});
+
